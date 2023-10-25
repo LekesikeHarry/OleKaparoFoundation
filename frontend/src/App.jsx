433d94edf05ab273a548b2e83384039b9550cd75
HarryLekishon <harry.lekesike@strathmore.edu>
2023-10-25 15:57:17 +0300

    error page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Login from "./pages/Login&Registration/Login";
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
 
 function App() {
   const router = createBrowserRouter([
@@ -19,11 +20,16 @@ function App() {
           path: "/about",
           element: <About />,
         },
+        
       ],
     },
     {
       path: "/login",
       element: <Login />,
+    },
+    {
+      path: "/error",
+      element: <ErrorPage />,
     },
   ]);
 
